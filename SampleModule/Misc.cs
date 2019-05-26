dc2ab7910f66705be474637e51bee7541c4c5403
EarsKilla <EarsKilla@users.noreply.github.com>
2019-05-26 23:20:32 +0300

    misc

---
+++ SampleModule/Misc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleModule
+{
+    public static class Misc
+    {
+        public static object ConfigSync = new object(); // for sync config across all parts of module
+    }
+}
