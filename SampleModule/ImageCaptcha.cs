597d2938587910d5e3d86f19ba2236d178954635
EarsKilla <EarsKilla@users.noreply.github.com>
2019-05-26 23:14:49 +0300

    ModuleInitialize update for base interface changes

---
+++ SampleModule/ImageCaptcha.cs
@@ -0,0 +1,29 @@
+﻿using SACModuleBase;
+using SACModuleBase.Models;
+using SACModuleBase.Models.Capcha;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleModule
+{
+    public class ImageCaptcha : ISACHandlerCaptcha
+    {
+        public bool ModuleEnabled { get; set; } = true;
+        public string ModuleName => "Sample image captcha";
+        public Version ModuleVersion => new Version("1.0.0.0");
+
+        public void ModuleInitialize(SACInitialize initialize) { }
+
+        public CaptchaResponse Solve(CaptchaRequest request)
+        {
+            var ss = new SACModuleBase.Models.SACInitialize()
+            {
+                ConfigurationPath = ""
+            };
+            throw new NotImplementedException();
+        }
+    }
+}
