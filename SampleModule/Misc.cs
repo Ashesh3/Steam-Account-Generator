bab821282750f890bac9839d4e69d79b88a1fa34
EarsKilla <EarsKilla@users.noreply.github.com>
2019-05-27 00:01:47 +0300

    Image captcha on 2captcha API example

--- SampleModule/Misc.cs
+++ SampleModule/Misc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,17 @@ namespace SampleModule
     public static class Misc
     {
         public static object ConfigSync = new object(); // for sync config across all parts of module
+
+        public static string[] TwoCaptcha(string resource, Dictionary<string, object> args)
+        {
+            var _client = new RestClient("http://2captcha.com/");
+            var srequest = new RestRequest(resource, Method.POST);
+            foreach (var key in args)
+            {
+                srequest.AddParameter(key.Key, key.Value);
+            }
+            var responsecap = _client.Execute(srequest);
+            return responsecap.Content.Split(new string[] { "|" }, StringSplitOptions.None);
+        }
     }
 }
