e7a0aa5050b4abb543bc1ee6a0e115f84543ec3f
EarsKilla <EarsKilla@users.noreply.github.com>
2019-05-27 01:03:24 +0300

    mailbox with config

--- SampleModule/MailBox.cs
+++ SampleModule/MailBox.cs
@@ -12,44 +12,54 @@ namespace SampleModule
     [Guid("C89C87D9-D9FE-447B-85A6-92203F0C3397")]
     public class MailBox : SACModuleBase.ISACHandlerMailBox
     {
-        private const string IMAP_HOST = "imap.rambler.ru";
-        private const int IMAP_PORT = 993;
-        private const bool IMAP_SSL = true;
-        private const string IMAP_AUTH_LOGIN = ""; // mail
-        private const string IMAP_AUTH_PASSW = ""; // password
-
         public bool ModuleEnabled { get; set; } = true;
-        public string ModuleName => "IMAP rambler mail handler";
+        public string ModuleName => "IMAP mail handler";
         public Version ModuleVersion => new Version("1.0.0.0");
 
+        private ConfigManager<Models.MailConfig> Config;
+        public void ModuleInitialize(SACInitialize initialize)
+        {
+            Config = new ConfigManager<Models.MailConfig>(initialize.ConfigurationPath, "mail.json", new Models.MailConfig());
+            if (!Config.Load())
+                Config.Save();
+        }
+
         public MailBoxResponse GetMailBox(MailBoxRequest request)
-            => new MailBoxResponse(IMAP_AUTH_LOGIN);
+        {
+            if (!Config.Load())
+            {
+                Config.Save();
+                return null;
+            }
+            return new MailBoxResponse(Config.Running.Email);
+        }
 
         public IReadOnlyCollection<MailBoxMailItem> GetMails(MailBoxResponse response)
         {
             var mails = new List<MailBoxMailItem>();
-            using (var client = new ImapClient())
+            if (Config.Load())
             {
-                client.Connect(IMAP_HOST, IMAP_PORT, IMAP_SSL);
-                client.Authenticate(IMAP_AUTH_LOGIN, IMAP_AUTH_PASSW);
-
-                var inbox = client.Inbox;
-                inbox.Open(MailKit.FolderAccess.ReadWrite);
-                var unreadIndex = inbox.FirstUnread;
-                if (unreadIndex > 0)
+                using (var client = new ImapClient())
                 {
-                    var inboxMail = inbox.GetMessage(unreadIndex);
-                    inbox.SetFlags(unreadIndex, MailKit.MessageFlags.Seen, true);
-                    var mail = new MailBoxMailItem(inboxMail.From?.FirstOrDefault()?.Name ?? "unknown",
-                        inboxMail.To?.FirstOrDefault()?.Name ?? "unknown",
-                        inboxMail.Subject ?? "none",
-                        inboxMail.HtmlBody ?? inboxMail.TextBody ?? "none");
-                    mails.Add(mail);
+                    client.Connect(Config.Running.Host, Config.Running.Port, Config.Running.UseSsh);
+                    client.Authenticate(Config.Running.Email, Config.Running.Password);
+
+                    var inbox = client.Inbox;
+                    inbox.Open(MailKit.FolderAccess.ReadWrite);
+                    var unreadIndex = inbox.FirstUnread;
+                    if (unreadIndex > 0)
+                    {
+                        var inboxMail = inbox.GetMessage(unreadIndex);
+                        inbox.SetFlags(unreadIndex, MailKit.MessageFlags.Seen, true);
+                        var mail = new MailBoxMailItem(inboxMail.From?.FirstOrDefault()?.Name ?? "unknown",
+                            inboxMail.To?.FirstOrDefault()?.Name ?? "unknown",
+                            inboxMail.Subject ?? "none",
+                            inboxMail.HtmlBody ?? inboxMail.TextBody ?? "none");
+                        mails.Add(mail);
+                    }
                 }
             }
             return new ReadOnlyCollection<MailBoxMailItem>(mails);
         }
-
-        public void ModuleInitialize(SACInitialize initialize) { }
     }
 }
---
+++ SampleModule/Models/MailConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleModule.Models
+{
+    public class MailConfig
+    {
+        public string Email { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string Host { get; set; } = "";
+        public int Port { get; set; } = 995;
+        public bool UseSsh { get; set; } = true;
+    }
+}
