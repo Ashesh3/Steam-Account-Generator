ff3f450f6e146c25fdc7b3d2bd2ee1049034207e
EarsKilla <EarsKilla@users.noreply.github.com>
2019-06-01 19:28:46 +0300

    assembly info updates

--- SteamAccCreator/Properties/AssemblyInfo.cs
+++ SteamAccCreator/Properties/AssemblyInfo.cs
@@ -1,13 +1,9 @@
-﻿using System.Resources;
-using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen über eine Assembly werden über die folgenden
-// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-// die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("SteamAccountGenerator")]
-[assembly: AssemblyDescription("Steam Account Generator")]
+[assembly: AssemblyDescription("Steam Account Generator. Maintained by EarsKilla https://github.com/EarsKilla/")]
 #if DEBUG
 [assembly: AssemblyConfiguration("DEBUG")]
 #elif PRE_RELEASE
@@ -17,29 +13,15 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("@DedSec1337")]
 [assembly: AssemblyProduct("Steam Account Generator")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
-// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
-// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
 [assembly: ComVisible(false)]
-
-// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
 [assembly: Guid("1d27f487-c3e3-4f50-9d1e-6c0994280b79")]
 
-// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-//
-//      Hauptversion
-//      Nebenversion
-//      Buildnummer
-//      Revision
-//
-// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
-// übernehmen, indem Sie "*" eingeben:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.2")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+// Major.Minor.Build
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
